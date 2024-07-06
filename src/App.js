415b67e2ae1e122bd69b6ddb2bd6fc02dbfcc25e
nitesh4447 <patharkarnitesh16@gmail.com>
2024-07-06 13:41:54 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [name, setname] = useState("")
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+  const [city, setcity] = useState("")
+  const [male, setmale] = useState("")
+  const [married, setmarried] = useState(false)
+
+
+  const handle = (e) => {
+    e.preventDefault()
+
+    let userdata = {
+      name,
+      email,
+      password,
+      male,
+      city,
+      married
+    }
+    console.log(userdata)
+
+    setcity("")
+    setemail("")
+    setmale("")
+    setmarried(false)
+    setpassword("")
+    setname("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <form action="" onSubmit={(e) => handle(e)}>
+          <input type="text" placeholder='Full Name' value={name} onChange={(e) => setname(e.target.value)} /> <br /> <br />
+          <input type="email" placeholder='Email' value={email} onChange={(e) => setemail(e.target.value)} /> <br /> <br />
+          <input type="password" placeholder='Password' value={password} onChange={(e) => setpassword(e.target.value)} /> <br /> <br />
+          <select name="" id="" value={city} onChange={(e) => setcity(e.target.value)}>
+            <option value="" >grade</option>
+            <option value="Freshman">Freshman</option>
+            <option value="Sophomore">Sophomore</option>
+            <option value="Junior">Junior</option>
+            <option value="Senior">Senior</option>
+          </select>  <br /> <br />
+          male: <input type="radio" name='gender' value={male} onChange={() => setmale("male")} />
+          female: <input type="radio" name='gender' value={male} onChange={(e) => setmale("female")} />  <br /> <br />
+         
+          <button>submit</button>
+        </form>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
