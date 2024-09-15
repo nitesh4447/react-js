b42ca0fb5e6d41de50e44e58f26a7cf44850412e
nitesh4447 <patharkarnitesh16@gmail.com>
2024-09-15 14:58:27 +0530

    update-1

---
+++ Event Handler/Event-handler/src/component/Post.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import './post.css'
+
+const Post = ({title,body}) => {
+  return (
+    <div>
+         <div>
+          {
+            <div className='post'> 
+              <h1>{title}</h1> 
+              <h3>{body}</h3>
+              </div>
+          }
+          
+         </div>
+    </div>
+  )
+}
+
+export default Post
